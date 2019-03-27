5c438566b5f56b0651bd51aebcebfb9b1cc6ccaf
Darrion Gering and Kaya Jepson <darrion@>
2019-03-27 15:47:16 -0700

    Add content to index.html and click functions to scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $("button#typewriter").click(function(){
+    var boughtTypewriter = 1;
+    console.log(boughtTypewriter);
+  });
+  $("button#piano").click(function(){
+    var boughtPiano = 1;
+    console.log(boughtPiano);
+  });
+  $("button#cup").click(function(){
+    var boughtCup = 1;
+    console.log(boughtCup);
+  });
+
+});
